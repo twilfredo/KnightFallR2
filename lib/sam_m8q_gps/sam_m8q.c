64fe36c4af43f42e92887db9adab0c7471cbac88
Thulith Wilfred MK <thulith.mallawa@uqconnect.edu.au>
2021-07-19 14:31:28 +1000

    Sam driver can receive multiple messages of arb length

--- lib/sam_m8q_gps/sam_m8q.c
+++ lib/sam_m8q_gps/sam_m8q.c
@@ -20,6 +20,67 @@
 
 LOG_MODULE_REGISTER(SAM_M8Q, LOG_LEVEL_DBG);
 
+/* UART RING BUFFERS */
+#define RING_BUF_SIZE (64 * 2)
+uint8_t rb_buf_sam[RING_BUF_SIZE];
+struct ring_buf rx_rb_sam;
+
+/* SAM Send/Rec Semaphore */
+K_SEM_DEFINE(samRecSem, 0, 1);
+
+/**
+ * @brief Callback function to read data from the modem, and save bytes to allocated
+ *          ring buffer. 
+ * 
+ * @param uart_device 
+ * @param user_data 
+ */
+static void uart_cb(const struct device *uart_device, void *user_data)
+{
+    ARG_UNUSED(user_data);
+    /* Verify uart_irq_update() */
+    if (!uart_irq_update(uart_device))
+    {
+        LOG_ERR("UART IRQ Update ERR");
+        return;
+    }
+    //const struct device *dev_uart1 = device_get_binding(UART1);
+    const struct device *dev_uart0 = uart_device;
+
+    int rx, ret;
+    static uint8_t read_buf[MAX_READ_SIZE];
+
+    /* get all of the data off UART as fast as we can */
+    while (uart_irq_update(dev_uart0) &&
+           uart_irq_rx_ready(dev_uart0))
+    {
+        rx = uart_fifo_read(dev_uart0, read_buf, sizeof(read_buf));
+
+        if (read_buf[0] == '\n')
+        {
+            //String is newline terminated, a data set is received.
+            k_sem_give(&samRecSem);
+        }
+
+        if (rx > 0)
+        {
+            ret = ring_buf_put(&rx_rb_sam, read_buf, rx);
+
+            if (ret != rx)
+            {
+                LOG_ERR("Rx buffer doesn't have enough space. "
+                        "Bytes pending: %d, written: %d",
+                        rx, ret);
+                sam_mdm_receiver_flush(uart_device);
+                //k_sem_give(&modemReadOkSem);
+                break;
+            }
+            //Modem Response received
+            //k_sem_give(&modemReadOkSem);
+        }
+    }
+}
+
 /**
  * @brief Initialize uart0 for sam_m8q GPS module communication
  * 
@@ -28,7 +89,7 @@ LOG_MODULE_REGISTER(SAM_M8Q, LOG_LEVEL_DBG);
 int sam_m8q_uart_init(void)
 {
     const struct uart_config uart_cfg = {
-        .baudrate = 115200,
+        .baudrate = 9600,
         .parity = UART_CFG_PARITY_NONE,
         .stop_bits = UART_CFG_STOP_BITS_1,
         .data_bits = UART_CFG_DATA_BITS_8,
@@ -43,6 +104,10 @@ int sam_m8q_uart_init(void)
     }
     else
     {
+        uart_irq_rx_disable(dev_uart0);
+        uart_irq_callback_user_data_set(dev_uart0, uart_cb, NULL);
+        uart_irq_rx_enable(dev_uart0);
+        ring_buf_init(&rx_rb_sam, 128, rb_buf_sam);
         return uart_configure(dev_uart0, &uart_cfg);
     }
 }
@@ -61,4 +126,81 @@ void sam_m8q_uart_tx(char *command)
     {
         uart_poll_out(uart_dev, command[i]);
     }
+}
+
+/**
+ * @brief Drains UART and Discards remaining data.
+ * 
+ * @param uart_device 
+ */
+void sam_mdm_receiver_flush(const struct device *uart_device)
+{
+    uint8_t c;
+
+    //Drain Uart
+    while (uart_fifo_read(uart_device, &c, 1) > 0)
+    {
+        continue;
+    }
+}
+
+/**
+ * @brief Controls and sends data to the SAM-M8Q GPS module. 
+ * 
+ */
+void thread_gps_ctrl(void)
+{
+
+    if (sam_m8q_uart_init())
+    {
+        LOG_ERR("UART Config Error");
+    }
+
+    while (1)
+    {
+        sam_m8q_uart_tx("Welcome\n\r");
+        k_sleep(K_SECONDS(1));
+    }
+}
+
+/**
+ * @brief Receives data from the SAM-M8Q GPS Module.
+ * 
+ */
+void thread_gps_receive(void)
+{
+
+    while (1)
+    {
+        k_sem_take(&samRecSem, K_FOREVER);
+        sam_recv();
+    }
+}
+
+/**
+ * @brief Receive data from sam gps ring buffer
+ * 
+ * @return true 
+ * @return false 
+ */
+bool sam_recv(void)
+{
+    int numReadBytes = 0; //Number of bytes read from ringbuffer
+    uint8_t ringBufferLoad[MAX_READ_SIZE];
+    char buffer[MAX_READ_SIZE];
+
+    numReadBytes = ring_buf_get(&rx_rb_sam, ringBufferLoad, sizeof(ringBufferLoad));
+
+    for (int i = 0; i < numReadBytes; ++i)
+    {
+        buffer[i] = ringBufferLoad[i];
+
+        if (i + 1 == numReadBytes)
+        {
+            buffer[i + 1] = '\0';
+        }
+    }
+
+    printk("%s", buffer);
+    return true;
 }
--- lib/sam_m8q_gps/sam_m8q.h
+++ lib/sam_m8q_gps/sam_m8q.h
@@ -1,11 +1,33 @@
 #ifndef SAM_M8Q_H
 #define SAM_M8Q_H
 
+/* ==================================================================== */
+/* ==============================THREAD DEFINES======================== */
+/* ==================================================================== */
+/* Debug Thread Stack size */
+#define STACK_SIZE_GPS_THREAD 2048
+
+/* ==================================================================== */
+/* ==============================MISC Defines========================== */
+/* ==================================================================== */
+#define MAX_READ_SIZE 128
+
+/* Debug Thread Priority */
+/* Lower Numerics has higher priority, -Ve Priorities are cooperitive threads, +Ve Priorities  are Preemtible  */
+#define THREAD_PRIORITY_GPS 3
+
 /* Device tree node identifier for UART0 - Connected to GPS Module*/
 #define UART0 DT_LABEL(DT_NODELABEL(uart0))
+
+void thread_gps_ctrl(void);
+
+void thread_gps_receive(void);
 
 int sam_m8q_uart_init(void);
 
 void sam_m8q_uart_tx(char *command);
 
+void sam_mdm_receiver_flush(const struct device *uart_device);
+
+bool sam_recv(void);
 #endif
